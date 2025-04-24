0c3f5a73add3c702670d9a9784557dc697cf37c7
Julia Shi <juliajshi@gmail.com>
2025-04-24 12:26:54 -0400

    maps hydration levels to percentage

---
+++ plant-v1/plant-v1.ino
@@ -0,0 +1,37 @@
+#include "Arduino_LED_Matrix.h"
+
+ArduinoLEDMatrix matrix;
+
+const int dry = 624;
+const int wet = 324;
+
+void setBits(uint8_t bits[8][12], int n) {
+  if (n < 0) n = 0;
+  if (n > 96) n = 96;
+
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 12; j++) {
+      int index = i * 12 + j;
+      bits[i][j] = (index < n) ? 1 : 0;
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  matrix.begin();
+}
+
+void loop() {
+  int sensorVal = analogRead(A2);
+  int percentageHumidity = map(sensorVal, wet, dry, 100, 0);
+
+  uint8_t bits[8][12];
+  setBits(bits, percentageHumidity);
+  matrix.renderBitmap(bits, 8, 12);
+
+  Serial.print(percentageHumidity);
+  Serial.println("%");
+
+  delay(100);
+}
